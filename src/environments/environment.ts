d78f6f1a8650a4d5caa22aa3469354dde5f51d6e
Jacinth <jacinthcedricbarral@gmail.com>
2026-05-20 15:14:45 +0800

    fix[Jacinth]: use bracket notation for environment variables — TypeScript required bracket notation for the process.env index signature — this satisfies the linter while still allowing the builder to perform build-time replacement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: process.env.NG_APP_API_URL || 'http://localhost:4000'
+  apiUrl: process.env['NG_APP_API_URL'] || 'http://localhost:4000'
 };
